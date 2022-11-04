8ec1109e3635ff3568bc4a4904c3c00b3a16e775
JacobLinCool <jacoblincool@gmail.com>
2022-11-05 00:20:33 +0800

    feat: adapters (unicourse, squash)

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,63 @@
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from "node:fs";
 import { resolve } from "node:path";
-import { clearLine, cursorTo } from "node:readline";
-import { Command } from "commander";
+import chalk from "chalk";
+import { program } from "commander";
+import cuid from "cuid";
+import log_progress from "log-update";
 import { CourseInfo, CourseMeta, DepartmentCode, query } from "ntnu-course";
+import { JsonCourse, JsonEntity, PackedJson } from "unicourse";
 import { Pool } from "@jacoblincool/puddle";
-import { Config } from "./types";
+import { date2term } from "./date";
 
 const departments = Object.keys(DepartmentCode).filter(
     (key) => key.match(/^[A-Z]/) && key.length < 5,
 ) as (keyof typeof DepartmentCode)[];
 
-const program = new Command()
+program
     .name("NTNU Course Crawler")
     .option("-c, --concurrency <num>", "concurrency number", "3")
-    .option("-y, --year <num>", "year", "110")
-    .option("-t, --term <num>", "term", "2")
-    .option("-s, --squash", "save squashed output", false)
-    .option("-f, --force", "overwrite existing output", false);
+    .option("-y, --year <num>", "year", date2term()[0].toString())
+    .option("-t, --term <num>", "term", date2term()[1].toString())
+    .option("-a, --adapter <adapter>", "use a schema adapter", "squash")
+    .option("-f, --force", "overwrite existing output", false)
+    .action(run)
+    .parse();
 
-program.action(run).parse(process.argv);
+async function run(opt: {
+    concurrency: string;
+    year: string;
+    term: string;
+    adapter: string;
+    force: boolean;
+}): Promise<void> {
+    const concurrency = parseInt(opt.concurrency);
+    const year = parseInt(opt.year);
+    const term = parseInt(opt.term);
+    const force = opt.force;
+    const adapter = opt.adapter;
 
-async function run(): Promise<void> {
-    const concurrency = parseInt(program.opts().concurrency);
-    const year = parseInt(program.opts().year);
-    const term = parseInt(program.opts().term);
-    const squash = program.opts().squash;
-    const force = program.opts().force;
-    await crawl({ concurrency, year, term, squash, force });
-}
-
-async function crawl({ concurrency, year, term, squash, force }: Config): Promise<void> {
     console.log(`NTNU Course Crawler. Target: ${year}-${term} Concurrency:`, concurrency);
 
     const START_TIME = Date.now();
-    const directory = resolve("data", `${year}-${term}`);
-    const squashed = resolve("data", "squashed");
+    const root = resolve("data", `${year}-${term}`);
     const counter = { meta: 0, meta_dep: 0, parsed: 0, skipped: 0, failed: 0 };
 
-    if (!existsSync(resolve(directory, "meta"))) {
-        mkdirSync(resolve(directory, "meta"), { recursive: true });
+    if (!existsSync(resolve(root, "meta"))) {
+        mkdirSync(resolve(root, "meta"), { recursive: true });
     }
 
     const meta_pool = new Pool(concurrency);
     const meta_map: Record<number, CourseMeta> = {};
 
-    departments.forEach((department) =>
+    for (const dep of departments) {
         meta_pool.push(async () => {
             try {
-                const filepath = resolve(directory, "meta", `${department}.json`);
+                const filepath = resolve(root, "meta", `${dep}.json`);
 
                 const exists = existsSync(filepath) && !force;
                 const meta: CourseMeta[] = exists
                     ? JSON.parse(readFileSync(filepath, "utf8"))
-                    : await query.meta({ year, term, department });
+                    : await query.meta({ year, term, department: dep });
 
                 if (!exists) {
                     writeFileSync(filepath, JSON.stringify(meta, null, 4));
@@ -67,18 +72,20 @@ async function crawl({ concurrency, year, term, squash, force }: Config): Promis
 
                 counter.meta_dep++;
             } catch (err) {
-                console.error((err as Error).message, { department });
+                console.error((err as Error).message, { dep });
             }
 
             log_progress(
-                `\x1b[93m[Preparing]\x1b[m \x1b[95m${time(
-                    Math.floor((Date.now() - START_TIME) / 1000),
-                )}\x1b[m ` +
-                    `Collecting metadata of \x1b[93m${department}\x1b[m (\x1b[93m${counter.meta}\x1b[m | ${counter.meta_dep}/${departments.length})`,
+                `${chalk.yellow("[Preparing]")} ${chalk.magenta(
+                    time(Math.floor((Date.now() - START_TIME) / 1000)),
+                )} ` +
+                    `Collecting metadata of ${chalk.yellow(dep)} (${chalk.yellow(counter.meta)} | ${
+                        counter.meta_dep
+                    }/${departments.length})`,
             );
-        }),
-    );
-    await meta_pool.go();
+        });
+    }
+    await meta_pool.run();
 
     const info_pool = new Pool(concurrency);
     const info_map: Record<number, CourseInfo> = {};
@@ -86,7 +93,7 @@ async function crawl({ concurrency, year, term, squash, force }: Config): Promis
     for (const meta of Object.values(meta_map)) {
         info_pool.push(async () => {
             try {
-                const dir = resolve(directory, "info", meta.department);
+                const dir = resolve(root, "info", meta.department);
                 if (!existsSync(dir)) {
                     mkdirSync(dir, { recursive: true });
                 }
@@ -112,47 +119,164 @@ async function crawl({ concurrency, year, term, squash, force }: Config): Promis
             }
 
             log_progress(
-                `\x1b[92m[Running]\x1b[m \x1b[95m${time(
-                    Math.floor((Date.now() - START_TIME) / 1000),
-                )}\x1b[m ` +
-                    `\x1b[96m${meta.code}\x1b[m ` +
-                    `Parsed: \x1b[93m${counter.parsed}\x1b[m, Skipped: \x1b[93m${counter.skipped}\x1b[m, Failed: \x1b[93m${counter.failed}\x1b[m`,
+                `${chalk.cyan("[Running]")} ${chalk.magenta(
+                    time(Math.floor((Date.now() - START_TIME) / 1000)),
+                )} ` +
+                    `${chalk.cyan(meta.code)} ` +
+                    `Parsed: ${chalk.yellow(counter.parsed)}, Skipped: ${chalk.yellow(
+                        counter.skipped,
+                    )}, Failed: ${chalk.red(counter.failed)}`,
             );
         });
     }
-    await info_pool.go();
+    await info_pool.run();
 
-    if (squash) {
-        if (!existsSync(squashed)) {
-            mkdirSync(squashed, { recursive: true });
+    if (adapter === "squash") {
+        const dir = resolve(root, "../squashed");
+        if (!existsSync(dir)) {
+            mkdirSync(dir, { recursive: true });
         }
-        const info_list = Object.values(info_map).sort((a, b) => a.serial - b.serial);
-        writeFileSync(
-            resolve(squashed, `${year}-${term}.json`),
-            JSON.stringify(info_list, null, 0),
-        );
+        const list = Object.values(info_map).sort((a, b) => a.serial - b.serial);
+        writeFileSync(resolve(dir, `${year}-${term}.json`), JSON.stringify(list, null, 0));
+    } else if (adapter === "unicourse") {
+        const dir = resolve(root, "../unicourse");
+        if (!existsSync(dir)) {
+            mkdirSync(dir, { recursive: true });
+        }
+
+        const ntnu: JsonEntity = {
+            name: "國立臺灣師範大學",
+            courses: [],
+            children: [],
+        };
+
+        const pack: PackedJson = {
+            teachers: [],
+            programs: [],
+            entities: [ntnu],
+        };
+
+        const courses = Object.values(info_map);
+
+        const teachers = new Map<string, string>();
+        for (const course of courses) {
+            for (const teacher of course.teachers) {
+                const key = `${course.department}-${teacher}`;
+                if (!teachers.has(key)) {
+                    teachers.set(key, teacher);
+                }
+            }
+        }
+        const teacher_rev = new Map<string, string>();
+        for (const [key, teacher] of teachers.entries()) {
+            const t = { id: cuid(), name: teacher };
+            pack.teachers.push(t);
+            teacher_rev.set(key, t.id);
+        }
+
+        const programs = new Set<string>();
+        for (const course of courses) {
+            for (const program of course.programs) {
+                if (!programs.has(program)) {
+                    programs.add(program);
+                }
+            }
+        }
+        const program_rev = new Map<string, string>();
+        for (const program of programs.values()) {
+            const p = { id: cuid(), name: program };
+            pack.programs.push(p);
+            program_rev.set(program, p.id);
+        }
+
+        const department_map = new Map<string, string>();
+        for (const [k, v] of Object.entries(DepartmentCode)) {
+            if (/[A-Z0-9]/.test(k) && /[^A-Z0-9]/.test(v)) {
+                department_map.set(v, k);
+            }
+        }
+
+        courses.sort((a, b) => a.department.length - b.department.length);
+
+        const deps = new Map<string, JsonEntity>();
+        for (const course of courses) {
+            const c: JsonCourse = {
+                id: cuid(),
+                name: course.name,
+                description: course.description,
+                code: course.code,
+                year: course.year,
+                term: course.term,
+                type:
+                    course.type === "必"
+                        ? "Compulsory"
+                        : course.type === "選"
+                        ? "Elective"
+                        : course.type === "通"
+                        ? "General"
+                        : "Other",
+                credit: course.credit,
+                teachers: course.teachers.map((t) => teacher_rev.get(`${course.department}-${t}`)!),
+                programs: course.programs.map((p) => program_rev.get(p)!),
+                prerequisites: [],
+                extra: {},
+            };
+            const skips = Object.keys(c);
+            for (const [k, v] of Object.entries(course)) {
+                if (!skips.includes(k)) {
+                    c.extra[k] = v;
+                }
+            }
+
+            if (!deps.has(course.department)) {
+                if (course.department.length <= 2) {
+                    const entity: JsonEntity = {
+                        name: department_map.get(course.department) || course.department,
+                        courses: [],
+                        children: [],
+                    };
+                    ntnu.children.push(entity);
+                    deps.set(course.department, entity);
+                } else {
+                    const parent = deps.get(course.department[0]);
+                    const entity: JsonEntity = {
+                        name: department_map.get(course.department) || course.department,
+                        courses: [],
+                        children: [],
+                    };
+
+                    if (parent) {
+                        parent.children.push(entity);
+                    } else {
+                        ntnu.children.push(entity);
+                    }
+                }
+            }
+
+            deps.get(course.department)?.courses.push(c);
+        }
+
+        writeFileSync(resolve(dir, `${year}-${term}.json`), JSON.stringify(pack, null, 0));
+    } else if (adapter) {
+        console.log(chalk.red(`Unknown adapter: ${adapter}`));
     }
 
     log_progress(
-        `\x1b[96m[Finished]\x1b[m \x1b[95m${time(
-            Math.floor((Date.now() - START_TIME) / 1000),
-        )}\x1b[m ` +
-            `Parsed: \x1b[93m${counter.parsed}\x1b[m, Skipped: \x1b[93m${counter.skipped}\x1b[m, Failed: \x1b[93m${counter.failed}\x1b[m\n`,
+        `${chalk.green("[Finished]")} ${chalk.magenta(
+            time(Math.floor((Date.now() - START_TIME) / 1000)),
+        )} ` +
+            `Parsed: ${chalk.yellow(counter.parsed)}, Skipped: ${chalk.yellow(
+                counter.skipped,
+            )}, Failed: ${chalk.red(counter.failed)}`,
     );
 }
+
 function time(seconds: number): string {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
     const seconds_ = seconds % 60;
 
     return `${hours}h ${minutes}m ${seconds_}s`;
-}
-
-function log_progress(message: string): void {
-    process.stdout.isTTY && clearLine(process.stdout, 0);
-    process.stdout.isTTY && cursorTo(process.stdout, 0);
-    process.stdout.write(message);
-    process.stdout.isTTY || process.stdout.write("\n");
 }
 
 function unique(course: CourseMeta): number {
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,1 @@
-export interface Config {
-    concurrency: number;
-    year: number;
-    term: number;
-    squash: boolean;
-    force: boolean;
-}
+export type Adapter = "squash" | "unicourse";
